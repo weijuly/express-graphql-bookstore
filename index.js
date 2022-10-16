f052239c10898567db0d83ec0d5e40b38f10735f
GopiKrishnan Ganesan <gopikrishnan.ganesan@gmail.com>
2022-10-16 08:29:13 +0530

    remove code

--- index.js
+++ index.js
@@ -82,45 +82,6 @@ const booksWithResolvers = [
 ]
 
 
-
-
-
-
-const booksX = [
-    // {
-    //     'id': 'c51f681d-8c83-4d50-a37b-bcecaaa0c1f3',
-    //     'title': 'The Pickwick Papers',
-    //     'year': 1836,
-    //     'isbn': '9783862678532',
-    //     'author': 'Charles Dickens'
-    // },
-    {
-        id: () => { 
-            console.log('fetching id'); 
-            return 'eea4a182-d809-4c4f-8e76-a1ddd2c079af'; 
-        },
-        title: () => {
-            console.log('fetching title');
-            return 'Oliver Twist';
-        },
-        year: () => {
-            console.log('fetching year');
-            return 1839;
-        },
-        isbn: () => {
-            console.log('fetching isbn');
-            return '9780140430172';
-        },
-        author: () => {
-            console.log('fetching author');
-            return 'Charles Dickens';
-        }
-    }
-
-]
-
-
-
 const resolvers = {
     getAuthors: () => authors,
     getAuthorsByName: ({name}) => authors.filter((author) => author === name),
